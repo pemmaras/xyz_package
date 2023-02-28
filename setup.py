ae8c94869fc17989a10b753ee5e27d335c78e317
Das Pemmaraju <dasc@ibm.com>
2023-02-28 11:31:24 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xyz_package',
+    version='1',
+    packages=['xyz_package'],
+    url='https://github.com/pemmaras/xyz_package',
+    license='MIT',
+    author='dasc',
+    author_email='dasc@ibm.com',
+    description='Test Repo'
+)
---
+++ xyz_package/xyz.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def calc_stuff(x):
+	y =0
+	if isinstance(x, int) or isinstance(x, float):
+		for i in range(10):
+			y += np.log(np.exp(x+i))
+	return y
